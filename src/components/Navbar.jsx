7ad78e2fa35993158ab664edfa7bbcdf4bc3e98a
Nikhil Sharma <nikhilsharma.csgeek@gmail.com>
2024-12-20 01:57:13 +0530

    Navbar Styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,37 @@
 import React from "react";
-import logo from '../assets/PortfolioLogo.png';
-import { FaLinkedin } from "react-icons/fa";
+import logo from "../assets/PortfolioLogo.png";
+import { RiLinkedinBoxLine } from "react-icons/ri";
 import { FaGithub } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 
 const Navbar = () => {
     return (
-        <nav className="mb-20 flex items-start justify-between py-6">
-            <div className="flex flex-shrink-0 items-center">
-                <img className="mx-2 w-10" src={logo} alt="" />
+        <nav className="mb-20 flex justify-between py-4">
+            <div className="flex flex-shrink-0 items-center cursor-pointer  border-b-2 border-transparent transition-all duration-300 hover:border-b-2 hover:border-amber-600 p-3 rounded-xl hover:scale-110">
+                <img className="mx-2 w-12" src={logo} alt="" />
             </div>
-            <div className="m-9 flex items-center justify-center gap-4 text-2xl">
-                <FaLinkedin></FaLinkedin>
-                <FaGithub></FaGithub>
-                <SiLeetcode></SiLeetcode>
+            <div className="m-4 flex items-center justify-center lg:gap-4 gap-1 text-5xl">
+                <span className="download-resume-btn text-xl text-amber-200 border-b-2 border-transparent transition-all duration-300 hover:border-b-2 hover:border-amber-600 p-3 rounded-xl cursor-pointer hover:scale-110">
+                    Download Resume
+                </span>
+                <a
+                    href="https://www.linkedin.com/in/nikhil-sharma-7b32601b6/"
+                    target="_blank"
+                >
+                    <RiLinkedinBoxLine className="p-2 cursor-pointer border-b-2 border-transparent transition-all duration-300 hover:border-b-2 hover:border-slate-300 hover:scale-110  rounded-xl"></RiLinkedinBoxLine>
+                </a>
+                <a
+                    href="https://github.com/nikhilsharma-github"
+                    target="_blank"
+                >
+                    <FaGithub className="p-2 cursor-pointer border-b-2 border-transparent transition-all duration-300 hover:border-b-2 hover:border-slate-300 hover:scale-110  rounded-xl"></FaGithub>
+                </a>
+                <a
+                    href="https://leetcode.com/u/NikhilSharma_LC/"
+                    target="_blank"
+                >
+                    <SiLeetcode className="p-2 cursor-pointer border-b-2 border-transparent transition-all duration-300 hover:border-b-2 hover:border-slate-300 hover:scale-110  rounded-xl"></SiLeetcode>
+                </a>
             </div>
         </nav>
     );
